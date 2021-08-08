b335612e29f25d4d0276ab0135cf37fba8795185
Evan Steinkerchner <esteinkerchner@gmail.com>
2021-08-07 22:59:05 -0400

    Initial commit

---
+++ src/main/java/me/roundaround/roundalib/RoundaLibMod.java
@@ -0,0 +1,9 @@
+package me.roundaround.roundalib;
+
+import net.fabricmc.api.ModInitializer;
+
+public final class RoundaLibMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
