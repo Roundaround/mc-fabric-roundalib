23f31318bae539b3d57febd64c8727c25b2dc1c0
Evan Steinkerchner <esteinkerchner@gmail.com>
2021-08-22 13:23:30 -0400

    Added accesswidener to utilize vanilla RecipesProvider

--- src/main/java/me/roundaround/roundalib/data/server/ModRecipesProvider.java
+++ src/main/java/me/roundaround/roundalib/data/server/ModRecipesProvider.java
@@ -1,12 +1,17 @@
 package me.roundaround.roundalib.data.server;
 
 import me.roundaround.roundalib.event.RecipeGenerateCallback;
-import me.roundaround.roundalib.mixin.RecipesProviderMixin;
 import net.minecraft.advancement.criterion.InventoryChangedCriterion;
+import net.minecraft.data.server.RecipesProvider;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
+import net.minecraft.predicate.NumberRange;
 import net.minecraft.predicate.item.ItemPredicate;
+import net.minecraft.tag.Tag;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 public abstract class ModRecipesProvider {
@@ -16,19 +21,51 @@ public abstract class ModRecipesProvider {
 
     public abstract void generate(Consumer<RecipeJsonProvider> exporter);
 
+    protected static void offerSingleOutputShapelessRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible input, @Nullable String group) {
+        RecipesProvider.offerSingleOutputShapelessRecipe(exporter, output, input, group);
+    }
+
+    protected static void offerShapelessRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible input, @Nullable String group, int outputCount) {
+        RecipesProvider.offerShapelessRecipe(exporter, output, input, group, outputCount);
+    }
+
+    protected static void offerSmelting(Consumer<RecipeJsonProvider> exporter, List<ItemConvertible> inputs, ItemConvertible output, float experience, int cookingTime, String group) {
+        RecipesProvider.offerSmelting(exporter, inputs, output, experience, cookingTime, group);
+    }
+
+    protected static void offerBlasting(Consumer<RecipeJsonProvider> exporter, List<ItemConvertible> inputs, ItemConvertible output, float experience, int cookingTime, String group) {
+        RecipesProvider.offerBlasting(exporter, inputs, output, experience, cookingTime, group);
+    }
+
+    protected static void offerStonecuttingRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible input) {
+        RecipesProvider.offerRecipe(exporter, output, input);
+    }
+
+    protected static void offerStonecuttingRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible input, int count) {
+        RecipesProvider.offerRecipe(exporter, output, input, count);
+    }
+
+    protected static InventoryChangedCriterion.Conditions conditionsFromItem(NumberRange.IntRange count, ItemConvertible item) {
+        return RecipesProvider.conditionsFromItem(count, item);
+    }
+
     protected static InventoryChangedCriterion.Conditions conditionsFromItem(ItemConvertible item) {
-        return RecipesProviderMixin.invokeConditionsFromItem(item);
+        return RecipesProvider.conditionsFromItem(item);
+    }
+
+    protected static InventoryChangedCriterion.Conditions conditionsFromTag(Tag<Item> tag) {
+        return RecipesProvider.conditionsFromTag(tag);
     }
 
     protected static InventoryChangedCriterion.Conditions conditionsFromItemPredicates(ItemPredicate... items) {
-        return RecipesProviderMixin.invokeConditionsFromItemPredicates(items);
+        return RecipesProvider.conditionsFromItemPredicates(items);
     }
 
     protected static String hasItem(ItemConvertible item) {
-        return RecipesProviderMixin.invokeHasItem(item);
+        return RecipesProvider.hasItem(item);
     }
 
     protected static String getItemPath(ItemConvertible item) {
-        return RecipesProviderMixin.invokeGetItemPath(item);
+        return RecipesProvider.getItemPath(item);
     }
 }
--- src/main/java/me/roundaround/roundalib/mixin/RecipesProviderMixin.java
+++ src/main/java/me/roundaround/roundalib/mixin/RecipesProviderMixin.java
@@ -1,13 +1,9 @@
 package me.roundaround.roundalib.mixin;
 
 import me.roundaround.roundalib.event.RecipeGenerateCallback;
-import net.minecraft.advancement.criterion.InventoryChangedCriterion;
 import net.minecraft.data.server.RecipesProvider;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
-import net.minecraft.item.ItemConvertible;
-import net.minecraft.predicate.item.ItemPredicate;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,25 +15,5 @@ public abstract class RecipesProviderMixin {
     @Inject(method = "generate(Ljava/util/function/Consumer;)V", at = @At("TAIL"))
     private static void generate(Consumer<RecipeJsonProvider> exporter, CallbackInfo callbackInfo) {
         RecipeGenerateCallback.EVENT.invoker().interact(exporter);
-    }
-
-    @Invoker
-    public static InventoryChangedCriterion.Conditions invokeConditionsFromItem(ItemConvertible item) {
-        throw new AssertionError();
-    }
-
-    @Invoker
-    public static InventoryChangedCriterion.Conditions invokeConditionsFromItemPredicates(ItemPredicate... items) {
-        throw new AssertionError();
-    }
-
-    @Invoker
-    public static String invokeHasItem(ItemConvertible item) {
-        throw new AssertionError();
-    }
-
-    @Invoker
-    public static String invokeGetItemPath(ItemConvertible item) {
-        throw new AssertionError();
     }
 }
