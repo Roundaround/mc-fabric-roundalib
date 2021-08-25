c942d2e2bd8d773a9a7d9882f99101cd8c3fad67
Evan Steinkerchner <esteinkerchner@gmail.com>
2021-08-25 18:53:23 -0400

    Reenabled mixins and test config screen. Scrollbar works!

--- src/main/java/me/roundaround/roundalib/RoundaLibMod.java
+++ src/main/java/me/roundaround/roundalib/RoundaLibMod.java
@@ -14,6 +14,6 @@ public final class RoundaLibMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-//        CONFIG.init();
+        CONFIG.init();
     }
 }
--- src/main/java/me/roundaround/roundalib/compat/modmenu/ModMenuImpl.java
+++ src/main/java/me/roundaround/roundalib/compat/modmenu/ModMenuImpl.java
@@ -8,7 +8,7 @@ import me.roundaround.roundalib.config.gui.ConfigScreen;
 public class ModMenuImpl implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return (screen) -> null;
-//        return (screen) -> new ConfigScreen(screen, RoundaLibMod.CONFIG);
+//        return (screen) -> null;
+        return (screen) -> new ConfigScreen(screen, RoundaLibMod.CONFIG);
     }
 }
