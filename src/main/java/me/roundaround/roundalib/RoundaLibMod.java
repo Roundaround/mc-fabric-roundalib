5d648451faf0f6ddc3e679cd91170063d69d6295
Evan Steinkerchner <esteinkerchner@gmail.com>
2021-08-30 19:57:21 -0400

    Removed dev test mod

--- src/main/java/me/roundaround/roundalib/RoundaLibMod.java
+++ src/main/java/me/roundaround/roundalib/RoundaLibMod.java
@@ -1,6 +1,5 @@
 package me.roundaround.roundalib;
 
-import me.roundaround.roundalib.config.RoundaLibConfig;
 import me.roundaround.roundalib.util.ModInfo;
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
@@ -10,10 +9,8 @@ public final class RoundaLibMod implements ModInitializer {
   public static final String MOD_ID = "roundalib";
   public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
   public static final ModInfo MOD_INFO = new ModInfo(MOD_ID, "0.0.3", 1);
-  public static final RoundaLibConfig CONFIG = new RoundaLibConfig();
 
   @Override
   public void onInitialize() {
-    CONFIG.init();
   }
 }
