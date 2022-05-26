9382dfa34dae7f7ea09dfca7c0d9277af3512c63
Evan Steinkerchner <esteinkerchner@gmail.com>
2022-05-26 01:13:40 -0400

    Updated gradle, readded ModMenu compat

--- src/main/java/me/roundaround/roundalib/RoundaLibMod.java
+++ src/main/java/me/roundaround/roundalib/RoundaLibMod.java
@@ -1,5 +1,6 @@
 package me.roundaround.roundalib;
 
+import me.roundaround.roundalib.config.RoundaLibConfig;
 import me.roundaround.roundalib.util.ModInfo;
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
@@ -9,8 +10,10 @@ public final class RoundaLibMod implements ModInitializer {
   public static final String MOD_ID = "roundalib";
   public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
   public static final ModInfo MOD_INFO = new ModInfo(MOD_ID, "0.0.5", 1);
+  public static final RoundaLibConfig CONFIG = new RoundaLibConfig();
 
   @Override
   public void onInitialize() {
+    CONFIG.init();
   }
 }
---
+++ src/main/java/me/roundaround/roundalib/compat/modmenu/ModMenuImpl.java
@@ -0,0 +1,13 @@
+package me.roundaround.roundalib.compat.modmenu;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.roundaround.roundalib.RoundaLibMod;
+import me.roundaround.roundalib.config.gui.ConfigScreen;
+
+public class ModMenuImpl implements ModMenuApi {
+  @Override
+  public ConfigScreenFactory<?> getModConfigScreenFactory() {
+    return (screen) -> new ConfigScreen(screen, RoundaLibMod.CONFIG);
+  }
+}
