37292c4385c48034ebf9c4946c6a3a8d6ff87536
Evan Steinkerchner <esteinkerchner@gmail.com>
2022-05-27 16:58:40 -0400

    Make toggle labels configurable

--- src/main/java/me/roundaround/roundalib/config/gui/control/ToggleControl.java
+++ src/main/java/me/roundaround/roundalib/config/gui/control/ToggleControl.java
@@ -6,22 +6,26 @@ import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 
 public class ToggleControl extends ButtonControl<BooleanConfigOption> {
+  private final String enabledI18nKey;
+  private final String disabledI18nKey;
+
   private Text cachedText;
 
-  public ToggleControl(
-      BooleanConfigOption configOption,
-      OptionRow parent,
-      int top,
-      int left,
-      int height,
-      int width) {
+  public ToggleControl(BooleanConfigOption configOption, OptionRow parent, int top, int left, int height, int width) {
+    this(configOption, parent, top, left, height, width, "config.toggle.enabled", "config.toggle.disabled");
+  }
+
+  public ToggleControl(BooleanConfigOption configOption, OptionRow parent, int top, int left, int height, int width,
+      String enabledI18nKey, String disabledI18nKey) {
     super(configOption, parent, top, left, height, width);
+    this.enabledI18nKey = enabledI18nKey;
+    this.disabledI18nKey = disabledI18nKey;
   }
 
   @Override
   public void init() {
     configOption.subscribeToValueChanges(this::onConfigValueChange);
-    cachedText = new TranslatableText(configOption.getValue() ? "config.toggle.enabled" : "config.toggle.disabled");
+    cachedText = new TranslatableText(configOption.getValue() ? enabledI18nKey : disabledI18nKey);
   }
 
   @Override
@@ -36,6 +40,6 @@ public class ToggleControl extends ButtonControl<BooleanConfigOption> {
   }
 
   private void onConfigValueChange(boolean prev, boolean curr) {
-    cachedText = new TranslatableText(curr ? "config.toggle.enabled" : "config.toggle.disabled");
+    cachedText = new TranslatableText(curr ? enabledI18nKey : disabledI18nKey);
   }
 }
