36d6edc05668d2d59017c0f84f585783d4e2982a
Evan Steinkerchner <esteinkerchner@gmail.com>
2023-03-30 14:39:11 -0400

    Package resources in separate zip

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-  id("fabric-loom") version "1.0-SNAPSHOT"
+  id("fabric-loom") version "1.1-SNAPSHOT"
+  id("distribution")
   id("maven-publish")
   id("com.github.johnrengelman.shadow") version "7.1.2"
 }
@@ -28,6 +29,16 @@ java {
   withSourcesJar()
 }
 
+distributions {
+  main {
+    contents {
+      from(tasks.processResources.get()) {
+        into("")
+      }
+    }
+  }
+}
+
 tasks.withType<JavaCompile> {
   options.encoding = "UTF-8"
   options.release.set(targetJavaVersion)
@@ -46,6 +57,10 @@ tasks.shadowJar {
   archiveVersion.set(fullVersion)
   archiveClassifier.set("shaded")
 
+  manifest {
+    attributes["Fabric-Loom-Remap"] = "true"
+  }
+
   relocate("com.electronwill.nightconfig", "me.roundaround.roundalib.shadow.nightconfig")
 }
 
@@ -57,19 +72,37 @@ tasks.remapJar {
   archiveVersion.set(fullVersion)
   archiveClassifier.set("")
 
-  addNestedDependencies.set(true)
+  manifest {
+    attributes["Fabric-Loom-Remap"] = "true"
+  }
 }
 
 tasks.remapSourcesJar {
   archiveBaseName.set(project.property("archive_base_name").toString())
   archiveVersion.set(fullVersion)
   archiveClassifier.set("sources")
+
+  manifest {
+    attributes["Fabric-Loom-Remap"] = "true"
+  }
+}
+
+tasks.distZip {
+  eachFile {
+    relativePath = RelativePath(true, *relativePath.segments.drop(3).toTypedArray())
+  }
+  includeEmptyDirs = false
+
+  archiveBaseName.set(project.property("archive_base_name").toString())
+  archiveVersion.set(fullVersion)
+  archiveClassifier.set("resources")
 }
 
 publishing {
   publications {
     create<MavenPublication>("maven") {
       from(components["java"])
+      artifact(tasks.distZip.get())
 
       artifactId = tasks.remapJar.get().archiveBaseName.get()
       version = tasks.remapJar.get().archiveVersion.get()
