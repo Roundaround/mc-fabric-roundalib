66abefe2cf629a84efd6129df2348dbb95497233
Evan Steinkerchner <esteinkerchner@gmail.com>
2023-04-26 23:52:53 -0400

    Update gradle plugin

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("roundalib") version "0.2.5"
+  id("roundalib") version "0.2.7"
 }
 
 dependencies {
--- testmod/build.gradle.kts
+++ testmod/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("roundalib") version "0.2.5"
+  id("roundalib") version "0.2.7"
 }
 
 val testModId = project.properties["mod_id"].toString()
