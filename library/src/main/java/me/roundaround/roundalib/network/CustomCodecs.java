d0cf408c64a74ffd7eede91a23fb1d1a8d4637a8
Roundaround <roundaroundna@gmail.com>
2024-07-02 10:25:39 -0400

    Added map-based custom codec helpers

--- library/src/main/java/me/roundaround/roundalib/network/CustomCodecs.java
+++ library/src/main/java/me/roundaround/roundalib/network/CustomCodecs.java
@@ -9,7 +9,9 @@ import net.minecraft.network.codec.ValueFirstEncoder;
 import net.minecraft.network.packet.CustomPayload;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Supplier;
 
 public final class CustomCodecs {
@@ -45,6 +47,58 @@ public final class CustomCodecs {
         for (V entry : list) {
           encoder.encode(entry, buf);
         }
+      }
+    };
+  }
+
+  public static <B extends PacketByteBuf, K, V> PacketCodec<B, Map<K, V>> forMap(
+      PacketCodec<PacketByteBuf, K> keyCodec,
+      final ValueFirstEncoder<B, V> valueEncoder,
+      final PacketDecoder<B, V> valueDecoder
+  ) {
+    return forMap((value, buf) -> keyCodec.encode(buf, value), keyCodec::decode, valueEncoder, valueDecoder);
+  }
+
+  public static <B extends PacketByteBuf, K, V> PacketCodec<B, Map<K, V>> forMap(
+      final ValueFirstEncoder<B, K> keyEncoder,
+      final PacketDecoder<B, K> keyDecoder,
+      PacketCodec<PacketByteBuf, V> valueCodec
+  ) {
+    return forMap(keyEncoder, keyDecoder, (value, buf) -> valueCodec.encode(buf, value), valueCodec::decode);
+  }
+
+  public static <B extends PacketByteBuf, K, V> PacketCodec<B, Map<K, V>> forMap(
+      PacketCodec<PacketByteBuf, K> keyCodec, PacketCodec<PacketByteBuf, V> valueCodec
+  ) {
+    return forMap((value, buf) -> keyCodec.encode(buf, value), keyCodec::decode,
+        (value, buf) -> valueCodec.encode(buf, value), valueCodec::decode
+    );
+  }
+
+  public static <B extends ByteBuf, K, V> PacketCodec<B, Map<K, V>> forMap(
+      final ValueFirstEncoder<B, K> keyEncoder,
+      final PacketDecoder<B, K> keyDecoder,
+      final ValueFirstEncoder<B, V> valueEncoder,
+      final PacketDecoder<B, V> valueDecoder
+  ) {
+    return new PacketCodec<>() {
+      @Override
+      public Map<K, V> decode(B buf) {
+        int size = buf.readInt();
+        Map<K, V> map = new HashMap<>(size);
+        for (int i = 0; i < size; i++) {
+          map.put(keyDecoder.decode(buf), valueDecoder.decode(buf));
+        }
+        return map;
+      }
+
+      @Override
+      public void encode(B buf, Map<K, V> map) {
+        buf.writeInt(map.size());
+        map.forEach((key, value) -> {
+          keyEncoder.encode(key, buf);
+          valueEncoder.encode(value, buf);
+        });
       }
     };
   }
