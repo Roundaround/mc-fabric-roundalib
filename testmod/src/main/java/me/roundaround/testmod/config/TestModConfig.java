9ad4e92d18b97d529bd13434b92e27265d4e5c6b
Roundaround <roundaroundna@gmail.com>
2024-07-03 16:20:11 -0400

    Lowercase TestModConfig option fields

--- testmod/src/main/java/me/roundaround/testmod/config/TestModConfig.java
+++ testmod/src/main/java/me/roundaround/testmod/config/TestModConfig.java
@@ -10,84 +10,84 @@ import me.roundaround.testmod.TestMod;
 import java.util.Arrays;
 
 public class TestModConfig extends ModConfig {
-  public final BooleanConfigOption FIRST;
-  public final BooleanConfigOption SECOND;
-  public final OptionListConfigOption<Difficulty> THIRD;
-  public final StringConfigOption FOURTH;
-  public final IntConfigOption FIFTH;
-  public final IntConfigOption SIXTH;
-  public final IntConfigOption SEVENTH;
-  public final FloatConfigOption EIGHTH;
-  public final FloatConfigOption NINTH;
-  public final PositionConfigOption TENTH;
-  public final PositionConfigOption ELEVENTH;
-  public final IntConfigOption TWELFTH;
-  public final BooleanConfigOption THIRTEENTH;
+  public final BooleanConfigOption first;
+  public final BooleanConfigOption second;
+  public final OptionListConfigOption<Difficulty> third;
+  public final StringConfigOption fourth;
+  public final IntConfigOption fifth;
+  public final IntConfigOption sixth;
+  public final IntConfigOption seventh;
+  public final FloatConfigOption eighth;
+  public final FloatConfigOption ninth;
+  public final PositionConfigOption tenth;
+  public final PositionConfigOption eleventh;
+  public final IntConfigOption twelfth;
+  public final BooleanConfigOption thirteenth;
 
   public TestModConfig() {
     super(TestMod.MOD_ID, options(TestMod.MOD_ID).setConfigVersion(2));
 
-    this.FIRST = this.registerConfigOption(
+    this.first = this.registerConfigOption(
         BooleanConfigOption.builder(this, "testOption0").setGroup("group0").setDefaultValue(true).build());
 
-    this.SECOND = this.registerConfigOption(BooleanConfigOption.builder(this, "testOption1")
+    this.second = this.registerConfigOption(BooleanConfigOption.builder(this, "testOption1")
         .setGroup("group0")
         .setDefaultValue(true)
-        .onUpdate((option) -> option.setDisabled(!this.FIRST.getPendingValue()))
+        .onUpdate((option) -> option.setDisabled(!this.first.getPendingValue()))
         .build());
 
-    this.THIRD = this.registerConfigOption(
+    this.third = this.registerConfigOption(
         OptionListConfigOption.builder(this, "testOption2", Arrays.stream(Difficulty.values()).toList())
             .setGroup("group0")
             .setDefaultValue(Difficulty.getDefault())
             .build());
 
-    this.FOURTH = this.registerConfigOption(StringConfigOption.builder(this, "testOption3")
+    this.fourth = this.registerConfigOption(StringConfigOption.builder(this, "testOption3")
         .setGroup("group0")
         .setDefaultValue("foo")
         .setMinLength(3)
         .setMaxLength(12)
-        .onUpdate((option) -> option.setDisabled(!this.FIRST.getPendingValue()))
+        .onUpdate((option) -> option.setDisabled(!this.first.getPendingValue()))
         .build());
 
-    this.FIFTH = this.registerConfigOption(IntConfigOption.builder(this, "testOption4")
+    this.fifth = this.registerConfigOption(IntConfigOption.builder(this, "testOption4")
         .setGroup("group1")
         .setDefaultValue(5)
         .setMinValue(0)
         .setMaxValue(100)
         .setStep(5)
         .addValidator((value, option) -> value % 25 != 0)
-        .onUpdate((option) -> option.setDisabled(!this.FIRST.getPendingValue()))
+        .onUpdate((option) -> option.setDisabled(!this.first.getPendingValue()))
         .build());
 
-    this.SIXTH = this.registerConfigOption(IntConfigOption.builder(this, "testOption5")
+    this.sixth = this.registerConfigOption(IntConfigOption.builder(this, "testOption5")
         .setGroup("group1")
         .setDefaultValue(5)
         .setMinValue(0)
         .setMaxValue(100)
         .setStep(null)
         .addValidator((value, option) -> value % 25 != 0)
-        .onUpdate((option) -> option.setDisabled(!this.FIRST.getPendingValue()))
+        .onUpdate((option) -> option.setDisabled(!this.first.getPendingValue()))
         .build());
 
-    this.SEVENTH = this.registerConfigOption(IntConfigOption.builder(this, "testOption6")
+    this.seventh = this.registerConfigOption(IntConfigOption.builder(this, "testOption6")
         .setGroup("group1")
         .setDefaultValue(5)
         .setMinValue(0)
         .setMaxValue(100)
         .setStep(5)
         .setUseSlider(true)
-        .onUpdate((option) -> option.setDisabled(!this.FIRST.getPendingValue()))
+        .onUpdate((option) -> option.setDisabled(!this.first.getPendingValue()))
         .build());
 
-    this.EIGHTH = this.registerConfigOption(FloatConfigOption.builder(this, "testOption7")
+    this.eighth = this.registerConfigOption(FloatConfigOption.builder(this, "testOption7")
         .setGroup("group1")
         .setDefaultValue(5)
         .setMinValue(0)
         .setMaxValue(100)
         .build());
 
-    this.NINTH = this.registerConfigOption(FloatConfigOption.builder(this, "testOption8")
+    this.ninth = this.registerConfigOption(FloatConfigOption.builder(this, "testOption8")
         .setGroup("group1")
         .setDefaultValue(5)
         .setMinValue(0)
@@ -95,15 +95,15 @@ public class TestModConfig extends ModConfig {
         .setUseSlider(true)
         .build());
 
-    this.TENTH = this.registerConfigOption(
+    this.tenth = this.registerConfigOption(
         PositionConfigOption.builder(this, "testOption9").setGroup("group2").build());
 
-    this.ELEVENTH = this.registerConfigOption(PositionConfigOption.builder(this, "testOption10")
+    this.eleventh = this.registerConfigOption(PositionConfigOption.builder(this, "testOption10")
         .setGroup("group2")
         .setDefaultValue(new Position(50, 50))
         .build());
 
-    this.TWELFTH = this.registerConfigOption(IntConfigOption.builder(this, "testOption11")
+    this.twelfth = this.registerConfigOption(IntConfigOption.builder(this, "testOption11")
         .setGroup("group2")
         .setDefaultValue(5)
         .setMinValue(0)
@@ -112,7 +112,7 @@ public class TestModConfig extends ModConfig {
         .setUseSlider(true)
         .build());
 
-    this.THIRTEENTH = this.registerConfigOption(
+    this.thirteenth = this.registerConfigOption(
         BooleanConfigOption.builder(this, "testOption12").setGroup("group3").setDefaultValue(true).build());
   }
 
