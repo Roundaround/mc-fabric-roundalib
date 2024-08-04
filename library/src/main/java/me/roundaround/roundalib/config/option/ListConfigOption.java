b3c30e5e796cce1ebc2e79fccbff4bf27e775684
Evan Steinkerchner <esteinkerchner@gmail.com>
2024-08-04 16:20:13 -0400

    Started working on List-based config options

---
+++ library/src/main/java/me/roundaround/roundalib/config/option/ListConfigOption.java
@@ -0,0 +1,24 @@
+package me.roundaround.roundalib.config.option;
+
+import me.roundaround.roundalib.config.ConfigPath;
+
+import java.util.List;
+
+public class ListConfigOption<T> extends ConfigOption<List<T>> {
+  protected ListConfigOption(Builder<T> builder) {
+    super(builder);
+  }
+
+  public static class Builder<T> extends ConfigOption.AbstractBuilder<List<T>, ListConfigOption<T>, Builder<T>> {
+    private Builder(ConfigPath path) {
+      super(path);
+
+      this.setDefaultValue(List.of());
+    }
+
+    @Override
+    protected ListConfigOption<T> buildInternal() {
+      return new ListConfigOption<>(this);
+    }
+  }
+}
