f3691fb2ce4b896b17fdee4bdc89272ecc39a695
Roundaround <roundaroundna@gmail.com>
2024-12-13 12:20:49 -0500

    Moved RoundaLib init to main entrypoint
    Conflicts:
    	library/build.gradle.kts
    	library/gradle.properties
    	testmod/build.gradle.kts

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-  id("io.github.goooler.shadow") version "8.1.7"
-  id("roundalib") version "0.7.0-SNAPSHOT"
+  id("fabric-loom") version "1.9-SNAPSHOT"
+  id("com.gradleup.shadow") version "9.0.0-beta2"
+  id("roundalib") version "0.9.0-SNAPSHOT"
 }
 
 val nightConfig: Configuration by configurations.creating {
