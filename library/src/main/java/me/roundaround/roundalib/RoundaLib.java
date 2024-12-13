fc7a58692b7ff25a293835e6e5adb6909d5c5f02
Roundaround <roundaroundna@gmail.com>
2024-12-13 11:11:16 -0500

    Moved RoundaLib init to main entrypoint

--- library/src/main/java/me/roundaround/roundalib/RoundaLib.java
+++ library/src/main/java/me/roundaround/roundalib/RoundaLib.java
@@ -1,15 +1,15 @@
 package me.roundaround.roundalib;
 
 import me.roundaround.roundalib.util.PathAccessor;
-import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public final class RoundaLib implements PreLaunchEntrypoint {
+public final class RoundaLib implements ModInitializer {
   public static final Logger LOGGER = LogManager.getLogger("roundalib");
 
   @Override
-  public void onPreLaunch() {
+  public void onInitialize() {
     PathAccessor.init();
   }
 }
