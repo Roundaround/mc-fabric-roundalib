7cb9733e4b892b729bb30e5019dd46e2bb3da527
Roundaround <roundaroundna@gmail.com>
2025-03-21 09:22:59 -0400

    Use Constants.MOD_ID in logger instance

--- src/main/java/me/roundaround/roundalib/RoundaLib.java
+++ src/main/java/me/roundaround/roundalib/RoundaLib.java
@@ -1,12 +1,13 @@
 package me.roundaround.roundalib;
 
+import me.roundaround.roundalib.generated.Constants;
 import me.roundaround.roundalib.util.PathAccessor;
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public final class RoundaLib implements ModInitializer {
-  public static final Logger LOGGER = LogManager.getLogger("roundalib");
+  public static final Logger LOGGER = LogManager.getLogger(Constants.MOD_ID);
 
   @Override
   public void onInitialize() {
