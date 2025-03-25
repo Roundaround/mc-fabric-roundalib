9ab3a006671c334ef595c5944fe7bf6110627ac9
Roundaround <roundaroundna@gmail.com>
2025-03-25 13:01:09 -0400

    Add annotation to modinitializer

--- modules/core/src/main/java/me/roundaround/roundalib/RoundaLib.java
+++ modules/core/src/main/java/me/roundaround/roundalib/RoundaLib.java
@@ -1,11 +1,13 @@
 package me.roundaround.roundalib;
 
 import me.roundaround.roundalib.generated.Constants;
+import me.roundaround.roundalib.gradle.api.annotation.Entrypoint;
 import me.roundaround.roundalib.util.PathAccessor;
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+@Entrypoint(Entrypoint.MAIN)
 public final class RoundaLib implements ModInitializer {
   public static final Logger LOGGER = LogManager.getLogger(Constants.MOD_ID);
 
